faaeefa36ae0fa53af1593648573ef23188a1585
VitorAlbu <vitoralbuqe@gmail.com>
2025-04-17 11:44:51 -0300

    arrumando o codigo

--- Documents/app.py
+++ Documents/app.py
@@ -6,7 +6,7 @@ import joblib
 
 # Carregar modelo treinado
 
-model = joblib.load('modelo_cardiaco.pkl')
+pipeline = joblib.load('modelo_cardiaco.pkl')
 
 st.set_page_config(page_title="Previsão de Risco Cardíaco", layout="centered")
 st.title("Previsão de Risco Cardíaco com IA")
@@ -26,30 +26,25 @@ oldpeak = st.number_input("Depressão ST (Oldpeak)", min_value=0.0, step=0.1)
 st_slope = st.selectbox("Inclinação do Segmento ST", ["Down", "Flat", "Up"])
 
 # Pré-processamento manual dos dados (codificação igual ao get_dummies com drop_first=True)
-input_data = {
+input_df = pd.DataFrame([{
     "Age": age,
+    "Sex": sex,
+    "ChestPainType": chest_pain,
     "RestingBP": restingbp,
     "Cholesterol": cholesterol,
-    "FastingBS": 1 if fastingbs == "Yes, y, yes, YES, s, SIM, sim, Sim" else 0,
+    "FastingBS": 1 if fastingbs == "Yes" else 0,
+    "RestingECG": resting_ecg,
     "MaxHR": maxhr,
+    "ExerciseAngina": exercise_angina,
     "Oldpeak": oldpeak,
-    "Sex_M": 1 if sex == "M, m" else 0,
-    "ChestPainType_ATA": 1 if chest_pain == "ATA, ata" else 0,
-    "ChestPainType_NAP": 1 if chest_pain == "NAP, nap" else 0,
-    "ChestPainType_TA": 1 if chest_pain == "TA, ta" else 0,
-    "RestingECG_LVH": 1 if resting_ecg == "LVH, lvh" else 0,
-    "RestingECG_ST": 1 if resting_ecg == "ST, st" else 0,
-    "ExerciseAngina_Y": 1 if exercise_angina == "Y, y, yes, YES, s, SIM, sim, Sim" else 0,
-    "ST_Slope_Flat": 1 if st_slope == "Flat" else 0,
-    "ST_Slope_Up": 1 if st_slope == "Up" else 0
-}
+    "ST_Slope": st_slope
+}])
 
 # Converter para DataFrame com as colunas que o modelo espera
-input_df = pd.DataFrame([input_data])
 
 # Botão de previsão
 if st.button("🔍 Analisar Risco"):
-    prediction = model.predict(input_df)[0]
+    prediction = pipeline.predict(input_df)[0]
     st.subheader("Resultado da Análise")
     if prediction == 1:
         st.error("🔴 Alto risco de doença cardíaca!")
