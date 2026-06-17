16b52966f5bb7fc9d14fa2812d4914351233ac99
treino258 <vitoralbuqe@gmail.com>
2026-06-17 20:21:34 -0300

    removeing cache

--- src/app/app.py
+++ src/app/app.py
@@ -15,8 +15,7 @@ import pandas as pd
 
 
 st.set_page_config(page_title="Previsão de Risco Cardíaco", layout="centered")
-# Carregar modelo treinado
-@st.cache_resource
+
 
 def main():
     artefato = _load_model()
